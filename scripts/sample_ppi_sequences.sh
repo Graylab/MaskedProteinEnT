f163cc3b441ce50d0ab266463ccd85810b843d2a
Sai Pooja Mahajan <14285703+heiidii@users.noreply.github.com>
2023-09-06 14:37:12 -0700

    Update sample_ppi_sequences.sh

--- scripts/sample_ppi_sequences.sh
+++ scripts/sample_ppi_sequences.sh
@@ -1,36 +1,13 @@
 #!/bin/bash -l
 
-module unload python
-module load cuda/11.1.0
-module load python/3.9.0
-module load git
+#ACTIVATE ENV
 
-module list
-
-source /home/smahaja4/repositories/clone_masked_model/venv_py39_torch19/bin/activate
-
-
-#### procs, gpus ###############
-n_proc=$SLURM_NTASKS_PER_NODE
-num_gpus=0
-env | grep -a SLURM | tee slurm_env
-qu="a100"
-if [ "$SLURM_JOB_PARTITION" = "$qu"  ]; then
-  IFS=','
-  read -a strarr <<< "$SLURM_STEP_GPUS"
-  num_gpus=${#strarr[*]}
-fi
-##############################
-
-
-MODEL=../trained_models/ProtPPIEnT_backup.ckpt
-TEST_RESULTS_BASE=/scratch16/jgray21/smahaja4_active/tmp_ppis/
-PDB_DIR=/scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/data/ppis
-PPI_PARTNERS_DICT=/scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/data/ppis/heteromers_partners_example.json
-python3 /scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/PPIAbAgSequenceSampler.py  \
-        --output_dir ${TEST_RESULTS_BASE} \
-        --num_gpus $num_gpus \
-        --num_procs $n_proc \
+MODEL=models/ProtPPIEnT_backup.ckpt
+OUTDIR=outdir
+PDB_DIR=data/ppis
+PPI_PARTNERS_DICT=data/ppis/heteromers_partners_example.json
+python3 PPIAbAgSequenceSampler.py  \
+        --output_dir ${OUTDIR} \
         --model $MODEL \
         --from_pdb $PDB_DIR \
 	--sample_temperatures 0.2,0.5 \
