ed887e48fd129c9925a311466d8e778135707e71
fatimadavila <fatima.a.davila@gmail.com>
2024-09-30 18:17:48 -0400

    tested and cleaned

--- scripts/sample_abag_sequences.sh
+++ scripts/sample_abag_sequences.sh
@@ -1,42 +1,35 @@
 #!/bin/bash -l
 
-module unload python
-module load cuda/11.1.0
-module load python/3.9.0
-module load git
+source <YOUR ENVIRONMENT> 
 
-module list
-
-source /home/smahaja4/repositories/clone_masked_model/venv_py39_torch19/bin/activate
-
-
-#### procs, gpus ###############
-n_proc=$SLURM_NTASKS_PER_NODE
-num_gpus=0
-env | grep -a SLURM | tee slurm_env
-qu="a100"
-if [ "$SLURM_JOB_PARTITION" = "$qu"  ]; then
-  IFS=','
-  read -a strarr <<< "$SLURM_STEP_GPUS"
-  num_gpus=${#strarr[*]}
+### CHECK/DOWNLOAD->DECOMPRESS PRETRAINED MODEL ###
+mkdir -p models
+if [ ! -f models/model.tar.gz ] && [ ! -f models/ProtAbAgEnT_backup.ckpt ]; then
+  wget -P models https://zenodo.org/records/8313466/files/model.tar.gz
 fi
-##############################
+tar --skip-old-files --strip-components=1 -C models -xvzf models/model.tar.gz models/ProtAbAgEnT_backup.ckpt
 
+### procs, gpus ###
+n_proc=6
+num_gpus=1
 
-MODEL=../trained_models/ProtAbAgEnT_backup.ckpt
-TEST_RESULTS_BASE=/scratch16/jgray21/smahaja4_active/tmp_abag/
-PDB_DIR=/scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/data/abag/
-PPI_PARTNERS_DICT=/scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/data/abag/1n8z_partners.json
-python3 /scratch16/jgray21/smahaja4_active/repositories/MaskedProteinEnT/PPIAbAgSequenceSampler.py  \
-        --output_dir ${TEST_RESULTS_BASE} \
-        --num_gpus $num_gpus \
-        --num_procs $n_proc \
-        --model $MODEL \
-        --from_pdb $PDB_DIR \
-	--sample_temperatures 0.2,0.5 \
-       	--num_samples 100 \
-	--partners_json ${PPI_PARTNERS_DICT} \
-	--partner_name Ab \
-        --antibody
- 
+echo "Running inference"
+date
 
+MODEL=models/ProtAbAgEnT_backup.ckpt
+OUTDIR=sampled_sequences
+PDB_DIR=data/abag/
+PPI_PARTNERS_DICT=data/abag/1n8z_partners.json
+SAMPLER_SCRIPT=PPIAbAgSequenceSampler.py
+# SAMPLER_SCRIPT=AntibodySequenceSampler.py
+python3 $SAMPLER_SCRIPT \
+  --output_dir ${OUTDIR} \
+  --model $MODEL \
+  --from_pdb $PDB_DIR \
+  --sample_temperatures 0.2,0.5 \
+  --num_samples 100 \
+  --partners_json ${PPI_PARTNERS_DICT} \
+  --partner_name Ab \
+  --antibody \
+  --num_gpus $num_gpus \
+  --num_procs $n_proc \
